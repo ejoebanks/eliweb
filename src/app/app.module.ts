b3dfa1cdc534bae3e1d1b03497658c65547396e9
ejoebanks <ejoebanks@gmail.com>
2019-08-02 13:46:56 -0500

    Added aboutme, main component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AboutmeComponent } from './aboutme/aboutme.component';
+import { MaincontentComponent } from './maincontent/maincontent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    AboutmeComponent
+    AboutmeComponent,
+    MaincontentComponent
   ],
   imports: [
     BrowserModule,
