eca5e8b8b8f1b4df55a8f8cd396bc1e3a07cc9a0
ejoebanks <ejoebanks@gmail.com>
2019-11-26 10:16:46 -0600

    Added component, proper routing methods

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MaincontentComponent} from './maincontent/maincontent.component';
+import {TestingComponent} from './testing/testing.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: MaincontentComponent
+  },
+  {
+  path: 'testing',
+  component: TestingComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
