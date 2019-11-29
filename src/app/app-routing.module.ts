885d57696107c2cddff5d099dcdcd60a11c8e240
ejoebanks <ejoebanks@gmail.com>
2019-11-29 17:42:56 -0600

    Added more component paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {MaincontentComponent} from './maincontent/maincontent.component';
 import {TestingComponent} from './testing/testing.component';
+import {OiComponent} from './oi/oi.component';
+import {HorseComponent} from './horse/horse.component';
+import {DogrescueComponent} from './dogrescue/dogrescue.component';
 
 
 const routes: Routes = [
@@ -13,7 +16,20 @@ const routes: Routes = [
   {
   path: 'testing',
   component: TestingComponent
+},
+{
+  path : 'oi',
+  component: OiComponent
+},
+{
+  path : 'horse',
+  component: HorseComponent
+},
+{
+  path : 'dogrescue',
+  component: DogrescueComponent
 }
+
 ];
 
 @NgModule({
